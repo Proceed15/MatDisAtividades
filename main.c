7406b67fe5074b7122e2c872af56222bc541e241
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-10-06 23:33:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-  int n, a = 0, b = 1, c, i;
+  //Parte de Fibonacci
+  int n, a = 0, b = 1, l = -1, c, i;
   printf("Digite o Número de Linhas da Sequência Fibonacci:\n");
   scanf("%d", &n);
   printf("Sequência Fibonacci: \n");
@@ -14,4 +15,18 @@ int main(void) {
   int p = a - b;
   int z = p * -1;
   printf("\nSaída do Último Termo: %d ", z);
+  //Parte de Recursividade
+  int rec;
+  printf("\n\nDigite o Número a ser Recursivo:\n");
+  scanf("%d", &rec);
+  int rar = rec;
+  printf("Saída do Número Recursivo:\n");
+  for (i = 0; i <= rar; i++) {
+    printf("%d ", rec);
+    int res = 1 * rec - 1;
+    rec = res;
+    if (rec <= 0) {
+      break;
+    }
+  }
 }
