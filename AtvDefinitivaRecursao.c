b176e883e1375b8a046e260b38185e8ec638b9d3
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-10-15 21:45:01 -0300

    Create AtvDefinitivaRecursao.c

---
+++ AtvDefinitivaRecursao.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+
+// Declaração da função multiplica
+int multiplica(int num1, int num2);
+void fatorial(int n, int resultado);
+
+int main(void) {
+  // Parte de Fatorial
+  int n = 0, a = 0, b = 1, c, i;
+  char resposta;
+  do {
+    do {
+      printf("Digite o Número a ser Fatorado:\n");
+      scanf("%d", &n);
+      if (n < 0) {
+        printf("\nEsse Fatorial só é valido para números reais maiores que "
+               "zero\n");
+      } else {
+        fatorial(n, 1);
+      }
+    } while (n < 0);
+    // Parte de Fibonacci
+    printf("\n\nDigite o Número de Linhas da Sequência Fibonacci:\n");
+    scanf("%d", &n);
+    printf("Sequência Fibonacci: \n");
+    for (i = 0; i <= n; i++) {
+      printf("%d ", a);
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    int p = a - b;
+    int z = p * -1;
+    printf("\nSaída do Último Termo: %d ", z);
+    // Parte da Multiplicação Recursiva:
+    int num1 = 0, num2 = 0;
+    char v[] = " vezes ";
+    printf("\n\nDigite o Primeiro Número a ser Recursivo:\n");
+    scanf("%d", &num1);
+    printf("\nDigite o Segundo Número a ser Recursivo:\n");
+    scanf("%d", &num2);
+    printf("Componentes da Multiplicação Recursiva:\n");
+    printf("%d", num1);
+    printf("%s", v);
+    printf("%d", num2);
+    printf("\n");
+    printf("\nSaída da Multiplicação Recursiva:\n");
+    printf("%d\n", multiplica(num1, num2));
+    // Parte da Soma Recursiva
+    int rec, sum = 0;
+    printf("\n\nDigite o Número a ser Recursivo:\n");
+    scanf("%d", &rec);
+    int rar = rec;
+    printf("Componentes do Número Recursivo:\n");
+    if (rar == 1) {
+      sum = 1;
+      printf("%d ", sum);
+    } else if (rar == 0) {
+      sum = 0;
+      printf("%d ", sum);
+    } else {
+      for (i = 0; i <= rar; i++) {
+        printf("%d ", rec);
+        int res = 1 * rec - 1;
+        sum += rec;
+        rec = res;
+        if (rec <= 0) {
+          break;
+        }
+      }
+    }
+    printf("\nSaída da Soma Recursiva:\n");
+    printf("%d", sum);
+
+    printf("\nDeseja Continuar? (S/N)\n");
+    scanf(" %c", &resposta);
+  } while (resposta == 'S' || resposta == 's' || resposta == 'Y' ||
+           resposta == 'y' || resposta == '1');
+}
+void fatorial(int n, int resultado) {
+  resultado = 1;
+  // Se 0 ou 1, 1
+  if (n == 0 || n == 1) {
+    printf("Fatorial: %d\n", resultado);
+    return;
+  }
+  for (int i = 1; i <= n; i++) {
+    resultado *= i;
+    printf("%d! = %d\n", i, resultado);
+  }
+  printf("\nSaída do Último Termo: %d ", resultado);
+  // fatorial(n - 1, resultado * n);
+}
+int multiplica(int num1, int num2) {
+  // multiplicação por zero é zero
+  if (num1 == 0 || num2 == 0) {
+    return 0;
+  } else if (num2 == 1) {
+    return num1;
+  } else {
+    return (num1 + multiplica(num1, num2 - 1));
+  }
+}
